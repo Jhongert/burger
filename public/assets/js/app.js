6271bf24ed62c59ab1f23e27c8498ed24f5d2a5e
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-11 10:26:15 -0400

    Fix app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		var burgerInput = $('#burger_name');
 		
 		if(burgerInput.val().trim() == ''){
+			$('.text-danger').remove();
 			burgerInput.after('<p class="text-danger">Please enter a burger name</p>');
 			burgerInput.focus();
 		} else {
